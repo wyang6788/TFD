1d7f7b4c337d3d54b981afa7fb8fc4dca64667e4
wyang6788@gmail.com <wyang6788@gmail.com>
2015-09-30 00:06:31 -0400

    Renamed the logo pngs and readded models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+	has_many :friendships
+	has_many :friends, :through => :friendships
+	has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+	has_many :inverse_friends, :through => :inverse_friendships, :source => :user	
+	has_many :playergames
+	has_many :users, through: :playergames
 end
